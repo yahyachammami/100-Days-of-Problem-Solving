21faedbf97dfc5d12d8d4a2c194321f894fecc4f
Chammami Yahia <chammemiyahia@gmail.com>
2025-01-27 17:17:30 +0100

    Search list array (#5)

---
+++ Day13- 134 Gas Station.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def canCompleteCircuit(self, gas, cost):
+        """
+        :type gas: List[int]
+        :type cost: List[int]
+        :rtype: int
+        """
+        if sum(gas)<sum(cost):
+            return -1
+        start = 0
+        tank = 0
+        for i in range(len(gas)):
+            tank += gas[i] - cost[i]
+            if tank < 0:
+                start = i + 1 
+                tank = 0
+        return start
+    
+# Input: gas = [1,2,3,4,5], cost = [3,4,5,1,2] Output: 3
