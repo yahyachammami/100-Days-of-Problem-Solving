770e2a1490683f39eede7f3a826cca7faa9a88fe
Chammami Yahia <chammemiyahia@gmail.com>
2025-01-30 15:26:09 +0100

    Sorting - Search - Array

---
+++ Day14- 135 Candy.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def candy(self, ratings):
+        """
+        :type ratings: List[int]
+        :rtype: int
+        """
+        n = len(ratings)
+        candies = [1] * n  # Step 1: Give each child at least 1 candy
+
+        # Left-to-right pass
+        for i in range(1, n):
+            if ratings[i] > ratings[i - 1]:
+                candies[i] = candies[i - 1] + 1
+
+        # Right-to-left pass
+        for i in range(n - 2, -1, -1):
+            if ratings[i] > ratings[i + 1]:
+                candies[i] = max(candies[i], candies[i + 1] + 1)
+
+        return sum(candies)  # Return the total candies needed
